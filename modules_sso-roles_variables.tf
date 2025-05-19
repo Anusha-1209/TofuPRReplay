d8f67794974c871e39a4c4125d130268a1813d0e
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-05-19 15:40:47 -0400

    merged_content

--- modules_sso-roles_variables.tf
+++ modules_sso-roles_variables.tf
@@ -6,8 +6,15 @@ variable "role_name" {
 variable "policy_arn" {
   description = "ARN of the PowerUserAccess AWS managed policy to attach to the role"
   type        = string
-  default = "arn:aws:iam::aws:policy/PowerUserAccess"
+  default     = "arn:aws:iam::aws:policy/PowerUserAccess"
 }
+
+variable "iam_read_only_policy_arn" {
+  description = "ARN of the IAMReadOnlyAccess AWS managed policy to attach to the venture roles"
+  type        = string
+  default     = "arn:aws:iam::aws:policy/IAMReadOnlyAccess"
+}
+
 variable "tags" {
   description = "Tags to apply to the IAM roles"
   type        = map(string)
